9efac1219036b4eab220857cb979be9951b6371a
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2023-04-01 01:30:22 -0300

    configurando index e gulp-image

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,36 @@ const image = require('gulp-image')
 
 function tarefasCss(gp) {
 
-  return gulp.src('./vendor/**/*.css')
-    .pipe(concat('libs.css'))
+  return gulp.src([
+    './node_modules/bootstrap/dist/css/bootstrap.css',
+    './vendor/owl/css/owl.css',
+    './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+    './vendor/jquery-ui/jquery-ui.css',
+    './src/css/style.css'
+  ])
+    .pipe(concat('styles.css'))
     .pipe(cssmin())
     .pipe(rename({ suffix: 'min' })) // libs.min.css
     .pipe(gulp.dest('./dist/css'))
 }
 
 function tarefasJS() {
-  return gulp.src('./vendor/**/*.js')
-    .pipe(concat('libs.js'))
+  return gulp.src([
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/bootstrap/dist/js/bootstrap.js',
+    './vendor/owl/js/owl.js',
+    './vendor/jquery-mask/jquery.mask.js',
+    './vendor/jquery-ui/jquery-ui.js',
+    './src/js/custom.js',
+    './src/js/efeitosJUI.js'
+  ])
+    .pipe(concat('scripts.js'))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' })) //libs.min.js
     .pipe(gulp.dest('./dist/js'))
 }
 
-function tarefaImagem() {
+function tarefasImagem() {
   return gulp.src('./src/images/*')
     .pipe(image({
       pngquant: true,
@@ -40,4 +54,4 @@ function tarefaImagem() {
 
 exports.styles = tarefasCss
 exports.scripts = tarefasJS
-exports.imagens = tarefaImagem
+exports.imagens = tarefasImagem
